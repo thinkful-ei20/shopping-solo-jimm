53559e9a0d68013340607977ffeb862d21f02096
unknown <ajameslusk@gmail.com>
2018-04-06 14:26:40 -0400

    changed store to an object, added filterUnchecked method, sortBy key

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 'use strict';
+
 // USER STORY 1: Shopping list should be rendered to page
-const STORE = [
-  {name: 'apples', checked: false},
-  {name: 'oranges', checked: false},
-  {name: 'milk', checked: true},
-  {name: 'bread', checked: false}
-];
+
+// const STORE = [
+//   {name: 'apples', checked: false},
+//   {name: 'oranges', checked: false},
+//   {name: 'milk', checked: true},
+//   {name: 'bread', checked: false}
+// ];
+
+const STORE = {
+  shoppingList: [
+    {name: 'apples', checked: false, time: new Date(Date.now() - 4)},
+    {name: 'oranges', checked: false, time: new Date(Date.now() - 3)},
+    {name: 'milk', checked: true, time: new Date(Date.now() - 2)},
+    {name: 'bread', checked: false, time: new Date(Date.now() - 1)}
+  ],
+  sortBy: 'alpha',
+  filterUnchecked: function(){
+    return this.shoppingList.filter(item => item.checked === false);
+  } 
+};
+
+function setSortBy(by){
+  STORE.sortBy = by;
+}
+
+function handleChangeSortBy(){
+  $('#sort-options').on('change', function(event){
+    //$(event.target).find('option:selected', )
+    setSortBy('time');
+  });
+}
+
 // Creates an html item
 const createHTMLItem = (item, index) => `
   <li class="js-item-index-element" data-item-index="${index}">
@@ -20,12 +47,14 @@ const createHTMLItem = (item, index) => `
       </div>
   </li>
 `;
+
 // Creates an html item for each item in the STORE
 const createHTMLList = (database) => database.map((item, index) => createHTMLItem(item, index)).join('');
+
 // Renders shopping list to the DOM
 const renderShoppingList = () => {
   console.log('`renderShoppingList` works like a charm');
-  $('.js-shopping-list').html(createHTMLList(STORE));
+  $('.js-shopping-list').html(createHTMLList(STORE.shoppingList));
 };
 
 // USER STORY 2: User should be able to add item to shopping list
@@ -33,7 +62,7 @@ const handleAddingItems = () => {
   $('#js-shopping-list-form').submit(function(event){
     event.preventDefault();
     const newItem = $('.js-shopping-list-entry').val();
-    STORE.push({name: newItem, checked: false});
+    STORE.shoppingList.push({name: newItem, checked: false, time: new Date(Date.now())});
     console.log(newItem);
     renderShoppingList();
   });
@@ -43,19 +72,24 @@ const handleAddingItems = () => {
 // USER STORY 4: User should be able to delete items from the list
 // Get index of checked or deleted item
 const getIndexOfItem = (event) => $(event.target).closest('.js-item-index-element').data('itemIndex');
+
 // Toggle checked item from the store
 const toggleCheckItem = (database, index) => { database[index].checked = !database[index].checked; };
+
 // Delete an item from the store
 const deleteItem = (database,index) => { database.splice(index, 1); };
+
 // Listen for clicks on check or delete button
 const checkOrDeleteItem = (typeOfButton, callbackFn) => {
   $('.js-shopping-list').on('click', typeOfButton, (event) => {
-    callbackFn(STORE, getIndexOfItem(event));
+    callbackFn(STORE.shoppingList, getIndexOfItem(event));
     renderShoppingList();
   });
 };
+
 // If user clicks check, toggles check item
 const handleCheckingItems = () => checkOrDeleteItem('.js-item-toggle', toggleCheckItem);
+
 // If user clicks delete, deletes item
 const handleDeletingItems = () => checkOrDeleteItem('.js-item-delete', deleteItem);
 
@@ -65,6 +99,7 @@ const handleShoppingList = () => {
   handleAddingItems();
   handleCheckingItems();
   handleDeletingItems();
+  handleChangeSortBy();
 };
 
 // call handle when DOM is ready
